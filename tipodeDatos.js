18a0df70b9a1689a7b85dc75bcac0932cd38b3d5
Gabriel <gabrielne310@gmail.com>
2025-06-24 23:13:47 -0300

    bigInt

--- tipodeDatos.js
+++ tipodeDatos.js
@@ -18,3 +18,64 @@ console.log(typeof "Bob"); // -> string
 let typeOfYear = typeof year;
 console.log(typeOfYear); // -> number
 console.log(typeof typeOfYear); // -> string
+
+//Booleano -llevan el prefijo "is" para indicar que se verifican valores verdaderos o falsos. 
+let  isDataValid  =  true;
+let  isStringTooLong  =  false;
+let  isGameOver  =  false;
+continueLoop  =  true;
+   
+console.log(false);  //  ->  false
+console.log(typeof  false);  //  ->  boolean
+console.log(isDataValid);  //  ->  true
+console.log(typeof  isDataValid);  //  ->  boolean
+
+//Numero Este es el tipo numérico principal en JavaScript que representa tanto números reales (p. ej., fracciones) como enteros
+const  año  =  1991;
+let  delayInSeconds  =  0.00016;
+let  area  =  (16  *  3.14);
+let  halfArea  =  area  /  2;
+   
+console.log(año);  //  ->  1991;
+console.log(typeof  año);  //  ->  number;
+
+let  a  =  10;  //  decimal  -  default  
+let  b  =  0x10;  //  hexadecimal  
+let  c  =  0o10;  //  octal  
+let  d  =  0b10;  //  binary  
+   
+console.log(a);  //  ->  10  
+console.log(b);  //  ->  16  
+console.log(c);  //  ->  8  
+console.log(d);  //  ->  2  
+   
+let  x  =  9e3;
+let  y  =  123e-5;
+console.log(x);  //  ->  9000
+console.log(y);  //  ->  0.00123
+
+
+let  e  =  1  /  0;
+let  f  =  -Infinity;
+   
+console.log(e);  //  ->  Infinity
+console.log(f);  //  ->  -Infinity
+console.log(typeof  e);  //  ->  number
+console.log(typeof  f);  //  ->  number
+   
+let  s  =  "it's  definitely  not  e  number";
+let  n  =  s  *  10;
+console.log(n);  //  ->  NaN
+console.log(typeof  n);  //  ->  number
+
+// BigInt
+// BigInt es un tipo de dato que permite trabajar con números enteros de precisión arbitraria
+let  big  =  1234567890000000000000n;
+let  big2  =  1n;
+   
+console.log(big);  //  ->  1234567890000000000000n
+console.log(typeof  big);  //  ->  bigint
+   
+console.log(big2);  //  ->  1n
+console.log(7n  /  4n);  //  ->  1n
+
