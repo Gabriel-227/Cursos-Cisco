ba26a4e2a6ab52a268840aa39cdb45f7f8bfcea5
Gabriel <gabrielne310@gmail.com>
2025-06-24 22:40:05 -0300

    tipos de datos

---
+++ tipodeDatos.js
@@ -0,0 +1,20 @@
+//Literales 
+let year = 2025;
+console.log(year);// -> 1990
+console.log(1991);// -> 1991
+console.log("Alice");// -> Alice
+
+// Operador typeof
+console.log(typeof year); // -> number
+
+//Probemos el operador typeof usando un ejemplo simple:
+console.log(typeof year); // -> number
+console.log(typeof 1990); // -> number
+   
+let name = "Alice";
+console.log(typeof name); // -> string
+console.log(typeof "Bob"); // -> string
+   
+let typeOfYear = typeof year;
+console.log(typeOfYear); // -> number
+console.log(typeof typeOfYear); // -> string
